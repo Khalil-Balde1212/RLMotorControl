21051198cf177840a733a38a3386113419a42944
Khalil-Balde1212 <Khalil.Immanuel@gmail.com>
2025-11-10 19:53:47 -0500

    Moved Files to Folders

--- MotorControl/src/motorControl.cpp
+++ MotorControl/src/motorControl.cpp
@@ -6,6 +6,20 @@ namespace Motor
     volatile int lastEncoded = 0;
     int countsPerRevolution = 1940;
 
+    float motorPos = 0.0f; //theta
+    float motorVel = 0.0f; //theta'
+    float motorAcc = 0.0f; //theta''
+    float motorJrk = 0.0f; //theta'''
+    float lastMotorPos = 0.0f; //theta_{t-1}
+    float lastMotorVel = 0.0f; //theta'_{t-1}
+    float lastMotorAcc = 0.0f; //theta''_{t-1}
+    float lastMotorJrk = 0.0f; //theta'''_{t-1}
+
+    float motorPosEMAGain = 0.1f;
+    float motorVelEMAGain = 0.1f;
+    float motorAccEMAGain = 0.1f;
+    float motorJrkEMAGain = 0.1f;
+
     int motorSpeed = 0;
 }
 
@@ -47,23 +61,31 @@ void Motor::encoderISR()
 void Motor::TaskEncoder(void *pvParameters)
 {
     (void)pvParameters;
+    int taskFrequencyHz = 100;
+    TickType_t delay = pdMS_TO_TICKS(1000 / taskFrequencyHz);
     // long lastPosition = 0;
 
     for (;;)
     {
+        TickType_t xLastWakeTime = xTaskGetTickCount();
         long currentPosition = encoderPosition;
         motorPos = (2.0f * 3.14159265f * currentPosition) / countsPerRevolution;
         motorPos = motorPosEMAGain * motorPos + (1.0f - motorPosEMAGain) * lastMotorPos;
         lastMotorPos = motorPos;
 
-
-        motorVel = (motorPos - lastMotorPos) / (0.1f); // Assuming 100ms interval
+        motorVel = (motorPos - lastMotorPos) / (0.1f);
         motorVel = motorVelEMAGain * motorVel + (1.0f - motorVelEMAGain) * motorVel;
-        // Serial.println(currentPosition);
+        lastMotorVel = motorVel;
 
+        motorAcc = (motorVel - lastMotorVel) / (0.1f);
+        motorAcc = motorAccEMAGain * motorAcc + (1.0f - motorAccEMAGain) * motorAcc;
+        lastMotorAcc = motorAcc;
 
-        TickType_t xLastWakeTime = xTaskGetTickCount();
-        vTaskDelayUntil(&xLastWakeTime, 100 / portTICK_PERIOD_MS);
+        motorJrk = (motorAcc - lastMotorAcc) / (0.1f);
+        motorJrk = motorJrkEMAGain * motorJrk + (1.0f - motorJrkEMAGain) * motorJrk;
+        lastMotorJrk = motorJrk;
+
+        vTaskDelayUntil(&xLastWakeTime, delay);
     }
 }
 
